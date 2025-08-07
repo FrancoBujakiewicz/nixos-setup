5badb42cceba885e3430785df1c512fa2cfbfbac
FrancoBujakiewicz <franconatahelbuja@gmail.com>
2025-08-07 15:24:43 -0300

    New keybindings

--- nyxt/keybindings.lisp
+++ nyxt/keybindings.lisp
@@ -1,14 +1,12 @@
- 
+
  (in-package #:nyxt-user)
 
  (define-configuration input-buffer
   ((override-map
     (let ((map (make-keymap "override-map")))
-      (define-key map "M-t" 'make-buffer-focus 
+      (define-key map "M-t" 'make-buffer-focus
                       "M-w" 'delete-current-buffer
-                      "M-1" 'switch-buffer-previous
-                      "M-2" 'switch-buffer-next
                       "M-f" 'follow-hint
-                      "M-backspace" 'history-backwards
-                      "M-keypadanter" 'history-forwards))))) ;; Enter = keypadanter
+                      "M-d" 'duplicate-buffer
+		      "M-b" 'set-url-from-bookmark)))))
 
